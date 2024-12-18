ed3accc08e6ae029f099e1a0a91b6c36d14a9d6b
NeoEx <draja.acc@gmail.com>
2024-12-18 14:25:31 -0300

    feat: add dinamic background, some styles, social login now workin

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent {
     this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
       .then((result) => {
         console.log('Usuário Logado:', result.user);
-       console.log('user', JSON.stringify(result.user));
+        localStorage.setItem('user', JSON.stringify(result.user));
       })
       .catch((error) => console.error('Erro no login:', error));
   }
