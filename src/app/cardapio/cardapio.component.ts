624676ac2da652d0313baa84de989ae04d7e5ecd
NeoEx <draja.acc@gmail.com>
2024-12-23 11:12:01 -0300

    feat: done fake api on menus.service, already done first step to apply that service on cardapio module

--- src/app/cardapio/cardapio.component.ts
+++ src/app/cardapio/cardapio.component.ts
@@ -1,14 +1,29 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MenuService } from 'src/services/menu.service';
+
 
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.component.html',
   styleUrls: ['./cardapio.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,  
 })
 export class CardapioComponent {
   readonly panelOpenState = signal(false);
-
   expansionIcon: string = this.panelOpenState() ? "keyboard_arrow_down" : "keyboard_arrow_up"
+
+  expansionMenus: any[] = [];
+  entradas: any[] = [];
+  pratosPrincipais: any[] = [];
+  massas: any[] = [];
+  carnes: any[] = [];
+  peixes: any[] = [];
+  sobremesas: any[] = [];
+  bebidas: any[] = [];
+
+  constructor(private menuService: MenuService) {}
+
+  ngOnInit(): void {
+    this.expansionMenus = this.menuService.getExpansionMenus()
+  }  
 }
